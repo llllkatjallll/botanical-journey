b8dd50026731578c1d83a2ce50ac7205bfbf2b26
llllkatjallll <katja.rempel@yahoo.de>
2022-10-03 23:14:03 +0200

    added html and css elements for journal ui

--- BotanicalJourney/Assets/Needle/Components.codegen/SelectPlant.cs
+++ BotanicalJourney/Assets/Needle/Components.codegen/SelectPlant.cs
@@ -8,7 +8,12 @@ namespace Needle.Typescript.GeneratedComponents
 	public partial class SelectPlant : UnityEngine.MonoBehaviour
 	{
 		public UnityEngine.Events.UnityEvent @onClick;
-		public float @currentNumber = 0f;
+		public string @plantName = "Blumen";
+		public string @plantLocation = "Blumen";
+		public string @plantDescription = "Blumen";
+		// public HTMLImageElement @image; → Could not resolve C# type
+		public float @currentNumber = 3f;
+		public void start(){}
 		public void onPointerClick(){}
 	}
 }
--- BotanicalJourney/Projects/CollaborativeSandbox/src/scripts/plantManager.ts
+++ BotanicalJourney/Projects/CollaborativeSandbox/src/scripts/plantManager.ts
@@ -5,19 +5,59 @@ import { serializeable } from "@needle-tools/engine/engine/engine_serialization_
 
 let x = 0;
 
+
 export class SelectPlant extends Behaviour
 {
     @serializeable(EventList)
     onClick?: EventList;
-    currentNumber: number = 0;
+    plantName: string = "Blumen";
+    plantLocation: string = "Blumen";
+    plantDescription: string = "Blumen";
+    
+    image?: HTMLImageElement
+    
+    currentNumber: number = 3;
+
+    
+    
+
+ 
+  
+
+    start(){
+        
+        const journalButton = document.getElementById("journalButton") as HTMLInputElement;
+        const journalContainer = document.getElementById("journal-container") as HTMLInputElement;
+        var journalActive = false;
+        journalButton.onclick = function(){
+
+            journalActive=!journalActive;
+            if(!journalActive){
+                journalContainer.classList.remove("show"); 
+                journalContainer.classList.add("hide");  
+
+            } else{
+                journalContainer.classList.remove("hide"); 
+                journalContainer.classList.add("show");    
+            }
+
+
+        };
+
+    }
     
 
     onPointerClick() {
         x++;
-
         const plantCounterPanel = document.getElementById("plant-counter") as HTMLInputElement;
+        const plantName = document.getElementById("plant-name") as HTMLInputElement;
+        if(plantCounterPanel)
         plantCounterPanel.innerHTML = x.toString();
         this.onClick?.invoke();
+
+        if(plantName){
+        plantName.innerHTML = this.plantName;
+        }
     }
 }
 
